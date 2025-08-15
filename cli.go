836e15985f87306d19fcb4d1358cf475338f9279
Boris Yakimov <yakimovb@gmail.com>
2025-08-15 15:54:58 +0300

    cleanup help menu

--- cli.go
+++ cli.go
@@ -114,97 +114,23 @@ example
 			return true, nil
 		}
 
-		// help show-total
-		if args[0] == "show-total" {
+		// help show/list
+		if args[0] == "show" || args[0] == "list" {
 			fmt.Printf(`
 Expense Tracking Tool
 Usage: 
 
-show-total                      - shows all transactions and P&Ls
-show-total <month> <year>       - shows transactions and P&L for specific month 
-show-total <year>               - shows only P&L for the specific year
+show and list are aliases and can be used interchangably
 
+show                      - shows all transactions and P&Ls
+show <month> <year>       - shows transactions and P&L for specific month 
+show <year>               - shows only P&L for the specific year
 
------------------------------------------------------------------------------------------------
+or 
 
-
-example:          show-total
-expected output:
-
-Year: 2025
-  Month: august
-
-    expense
-     1. €13.49    | food       | test food in next month
-     2. €200.00   | cash       | atm withdrawal
-     3. €12.30    | food       | tacos
-     4. €33.62    | food       | switch back to pizza
-
-    investment
-     1. €19.25    | insurance  | prorperty insurance august 2025
-     2. €78.00    | insurance  | life insurance august 2025
-
-    income
-     1. €1500.00  | salary     | income from employment
-
-
-p&l result: €1143.34 | 76.2%%
-
-  Month: july
-
-    expense
-     1. €53.40    | food       | groceries
-     2. €432.00   | renovation | fence
-     3. €45.00    | food       | burgers
-     4. €12.35    | food       | sandwitch
-     5. €100.00   | travel     | rent a car gas
-     6. €52.00    | food       | groceries
-
-    investment
-     1. €78.00    | insurance  | life insurance monthly payment
-     2. €19.25    | insurance  | property insurance monthly payment
-     3. €100.00   | funds      | ibkr etf investment
-
-    income
-     1. €1500.00  | salary     | income from employment
-
-
-p&l result: €608.00 | 40.5%%
-
-
------------------------------------------------------------------------------------------------
-
-
-example:          show-total 2025
-expected output:
-
-p&l result: €1751.34 | 58.4%%
-
-
------------------------------------------------------------------------------------------------
-
-
-example:          show-total july 2025
-expected output:
-
-	expense
-     1. €53.40    | food       | groceries
-     2. €432.00   | renovation | fence
-     3. €45.00    | food       | burgers
-     4. €12.35    | food       | sandwitch
-     5. €100.00   | travel     | rent a car gas
-     6. €52.00    | food       | groceries
-
-investment
-     1. €78.00    | insurance  | life insurance monthly payment
-     2. €19.25    | insurance  | property insurance monthly payment
-     3. €100.00   | funds      | ibkr etf investment
-
-income
-     1. €1500.00  | salary     | income from employment
-
-p&l result: €608.00 | 40.5%%
-
+list                      - shows all transactions and P&Ls
+list <month> <year>       - shows transactions and P&L for specific month 
+list <year>               - shows only P&L for the specific year
 `)
 			return true, nil
 		}
@@ -216,10 +142,10 @@ Expense Tracking Tool
 
 Usage:
 
-list                    List transactions
-show-total              Show totals of all transactions
+list                    List transactions - list (without arguments) ; list <month> <year> ; list <year>
+show                    An alias to the list command
 add:                    Add a transaction - add <transaction_type> <amount> <category> <description>
-delete (del)            Delete a transaction - delete <transaction_type> <transaction_id> (transaction IDs can be seen in list and show-total)
+delete (del)            Delete a transaction - delete <transaction_type> <transaction_id> (transaction IDs can be seen via the list command)
 update                  Update transaction - update <transaction_type> <transaction_id> <amount> <category> <description>
 help                    Display a help message
 exit                    Exit the expense-tracking tool
@@ -229,7 +155,8 @@ Detailed usage:
 help add                Get more info on add command 
 help delete (del)       Get more info on delete command
 help update             Get more info on update command
-help show-total         Get more info on show-total command
+help show               Get more info on show command
+help list               Get more info on list command
 `)
 	return true, nil
 }
